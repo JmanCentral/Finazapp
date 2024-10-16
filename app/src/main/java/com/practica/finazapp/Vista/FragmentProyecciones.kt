c49415b22aff6d9da0c03babc148f4cc3ae16e4b
jmancipet <jmancipet@ucentral.edu.co>
2024-10-16 12:20:46 -0500

    Avances

--- app/src/main/java/com/practica/finazapp/Vista/FragmentProyecciones.kt
+++ app/src/main/java/com/practica/finazapp/Vista/FragmentProyecciones.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.practica.finazapp.R
 import com.practica.finazapp.ViewModel.AlertaViewModel
 import com.practica.finazapp.ViewModel.GastosViewModel
 import com.practica.finazapp.ViewModel.IngresoViewModel
